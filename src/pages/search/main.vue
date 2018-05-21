7fa7a3f955d8f91047ca64c50e69aab1804d18ca
zxw <123844114@qq.com>
2018-05-21 23:33:14 +0800

    search dockerfile

--- src/pages/search/main.vue
+++ src/pages/search/main.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="main">
     <tabs :options='selected'></tabs>
-    <div class="con">主体内容</div>
+    <div class="con">
+      <div class="searchBar">
+        <input type="text" placeholder="搜索歌曲、歌单、专辑">
+        <span>搜索</span>
+      </div>
+      <div class="hotSearch">
+        <h2>热门搜索</h2>
+        <div class="hotWord">
+          <span>我是歌手第四季</span>
+          <span>我是歌手第四季</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +21,7 @@
 import tabs from '@/components/Tabs'
 export default {
   name: 'mainSearch',
-  components: {tabs},
+  components: { tabs },
   data () {
     return {
       selected: 'item3'
@@ -20,5 +32,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-
+.hotWord{
+  span{
+    padding 5px
+    border-radius 15px
+    color red
+    border 1px solid red
+  }
+}
 </style>
